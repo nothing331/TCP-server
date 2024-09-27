e16dab4473458185d7c667a3663143495d97d35b
Ayush <75002721+nothing331@users.noreply.github.com>
2024-09-28 03:20:00 +0530

    tcp server complete

--- main.go
+++ main.go
@@ -1,40 +1,105 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 )
 
-func main() {
-	l, err := net.Listen("tcp", "localhost:8081")
+type Message struct {
+	from    string
+	payload []byte
+}
+
+type Server struct {
+	listenAddr string
+	ln         net.Listener
+	quitch     chan struct{}
+	msgch      chan Message
+}
+
+func NewServer(listnerAddr string) *Server {
+	return &Server{
+		listenAddr: listnerAddr,
+		quitch:     make(chan struct{}),
+		msgch:      make(chan Message, 10),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	defer ln.Close()
+	s.ln = ln
 
-	defer l.Close()
+	go s.acceptLoop()
+
+	<-s.quitch
+	close(s.msgch)
+
+	return nil
+}
+
+func (s *Server) acceptLoop() {
 	for {
-		conn, err := l.Accept()
+		rep, err := s.ln.Accept()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("Error :", err)
+			continue
 		}
 
-		go handleReq(conn)
+		fmt.Println("new connection to the server", rep.RemoteAddr())
+
+		go s.getData(rep)
 	}
 }
 
-func handleReq(conn net.Conn) {
-	buf := make([]byte, 1024)
-
+func (s *Server) getData(rep net.Conn) {
+	defer rep.Close()
+	reader := bufio.NewReader(rep)
 	for {
-		resInt, err := conn.Read(buf)
+		line, err := reader.ReadString('\n')
 		if err != nil {
-			log.Fatal(err)
+			if err != io.EOF {
+				fmt.Println("Error:", err)
+			}
+			break
 		}
-		fmt.Printf("Recieved data (%s): %s\n", conn.LocalAddr(), string(buf[:resInt]))
-		conn.Write([]byte("Data Send."))
-
+		s.msgch <- Message{
+			from:    rep.RemoteAddr().String(),
+			payload: []byte(line),
+		}
 	}
-
-	// conn.Close()
 }
+
+func main() {
+	server := NewServer(":3000")
+
+	go func() {
+		for msg := range server.msgch {
+			fmt.Printf("message recived(%s):%s\n", msg.from, string(msg.payload))
+		}
+	}()
+
+	log.Fatal(server.Start())
+}
+
+// func handleReq(conn net.Conn) {
+// 	buf := make([]byte, 1024)
+
+// 	for {
+// 		resInt, err := conn.Read(buf)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		fmt.Printf("Recieved data (%s): %s\n", conn.LocalAddr(), string(buf[:resInt]))
+// 		conn.Write([]byte("Data Send."))
+
+// 	}
+
+// 	// conn.Close()
+// }
